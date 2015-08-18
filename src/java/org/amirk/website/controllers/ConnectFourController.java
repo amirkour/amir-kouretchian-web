e46e9c9ec98f06fa4cba785e96aa2dcbc1540eac
amir kouretchian <amirsgarbunk@gmail.com>
2015-08-17 20:54:31 -0700

    just wrote a big helper to render the html of a game board - still needs testing, but just checking it in to back it up!

--- src/java/org/amirk/website/controllers/ConnectFourController.java
+++ src/java/org/amirk/website/controllers/ConnectFourController.java
@@ -1,6 +1,8 @@
 package org.amirk.website.controllers;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedSet;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.amirk.games.connectfour.db.*;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
 @Controller
 @RequestMapping(value="/connectfour")
@@ -34,11 +38,6 @@ public class ConnectFourController extends BaseController {
     
     @RequestMapping(method = RequestMethod.GET)
     public String index(){ return "connectfour/index"; }
-    
-    @RequestMapping(method=RequestMethod.GET, value="/play/{gameId}")
-    public String playGame(@PathVariable("gameId") long gameId){
-        return "connectfour/play";
-    }
     
     @RequestMapping(method=RequestMethod.POST, value="/play")
     public String playNewGame(@RequestParam("boardWidth") int boardWidth,
@@ -72,6 +71,82 @@ public class ConnectFourController extends BaseController {
         this.daoGame.save(newGame);
         
         return this.redirect("/connectfour/play/" + newGame.getId());
+    }
+    
+    @RequestMapping(method=RequestMethod.GET, value="/play/{gameId}")
+    public String showGame(@PathVariable("gameId") long gameId){
+        return "connectfour/play";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, value="/play/{gameId}")
+    public String makeMoveForGame(){
+        return "foo";
+    }
+    
+    /*
+     * Helper that constructs the html string for the game board, replete
+     * with interactable buttons and all (where appropriate.)
+     *
+     * Constructing the equivlent html in the jsp would be ... ugly.
+     * Not that this is necessarily beautiful, but I think it's preferable
+     * to all the script in a jsp.
+     */
+    protected String getBoardHtmlFor(Game game, Player nextPlayerToMove, HttpServletRequest request) throws Exception {
+        if(game == null || nextPlayerToMove == null){ throw new Exception("Cannot render game board for missing or invalid game and/or player objects - please report this error!"); }
+        
+        long[][] board = game.getBoardMatrix();
+        if(board == null){ throw new Exception("Cannot render game board for missing or invalid board matrix - please report this error!"); }
+        
+        // as we render the board, we'll give the next player a button to push
+        // if we encounter a legal move (and assuming the next player to move
+        // is human.)
+        SortedSet<GameMove> legalMoves = game.getLegalMoves();
+        
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<table>");
+        
+        // the html table will render from top-to-bottom, left-to-right, so
+        // we have to flip-flop the row and column indices to get the layout right:
+        for(int j = 0; j < board[0].length; j++){
+            htmlBuilder.append("<tr>");
+            for(int i = 0; i < board.length; i++){
+                
+                // if the next player is human and this i,j spot is a legal move,
+                // give the player a form/button they can use at this spot to make
+                // a move
+                if(nextPlayerToMove.isConsideredHuman() && legalMoves.contains(new GameMove(i,j))){
+                
+                    UriComponents uriBuilder = ServletUriComponentsBuilder.fromContextPath(request)
+                                                                          .path("/connectfour/play/" + game.getId())
+                                                                          .build();
+                    htmlBuilder.append("<td>");
+                    htmlBuilder.append("<form method=\"POST\" action=\"" + uriBuilder.encode().toString() + "\" >");
+                    htmlBuilder.append("<input type=\"hidden\" name=\"row\" value=\"" + i + "\" />");
+                    htmlBuilder.append("<input type=\"hidden\" name=\"col\" value=\"" + j + "\" />");
+                    htmlBuilder.append("<input type=\"hidden\" name=\"playerId\" value=\"" + nextPlayerToMove.getId() + "\" />");
+                    htmlBuilder.append("<input type=\"submit\" value=\"X\" />");
+                    htmlBuilder.append("</form>");
+                    htmlBuilder.append("</td>");
+                    
+                // otherwise, just render this i,j spot normally. it's either a blank spot,
+                // or occupied by somebody who played here previously
+                }else{
+                
+                    String colorOfThisSpot = game.getColorForPlayerAt(i, j);
+                    if(StringUtils.isBlank(colorOfThisSpot)){
+                        htmlBuilder.append("<td>");
+                    }else{
+                        htmlBuilder.append("<td style=\"background-color: " + colorOfThisSpot + ";\" >");
+                    }
+                    
+                    htmlBuilder.append("</td>");
+                }
+            }
+            htmlBuilder.append("</tr>");
+        }
+        htmlBuilder.append("</table>");
+        
+        return htmlBuilder.toString();
     }
     
     /*
